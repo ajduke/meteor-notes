d665d8112ff93c2d4b1cfc4364949fc66b532fef
ajduke <ajduke@about.me>
2014-12-06 19:48:52 +0530

    restructured the code and improve some features

--- client/app.js
+++ client/app.js
@@ -1,12 +1,41 @@
+Notes = new Meteor.Collection("notes");
+Tags = new Meteor.Collection("tags");
+
 Deps.autorun(function() {
-  console.log('runnning Deps');
   // userId() can be changed before user(), because loading profile takes time
   if(Meteor.userId()) {
-//      Router.go('/notes');
     Session.set('userId',Meteor.userId());
   }else{
     Router.go('/');
     console.log('User logged out');
     Session.set('userId','');
+  }
+});
+
+Meteor.startup(function () {
+  Session.setDefault("selectedTag", null);
+  Session.set('current_template','')
+}); // end of startup
+
+
+Meteor.autorun(function() {
+
+  if (Meteor.userId()) {
+    Session.set('userId', Meteor.userId());
+    setTimeout(function () {
+      $('#noteTag').tagsinput('refresh');
+    }, 200)
+  } else {
+    Session.set('userId', '');
+  }
+});
+
+
+// Handlebars if eq helper definition
+Template.registerHelper('if_eq', function (ownerId) {
+  if (ownerId === Meteor.userId()) {
+    return true
+  } else {
+    return false
   }
 });
--- client/index.js
+++ client/index.js
@@ -1,88 +1,58 @@
-Notes = new Meteor.Collection("notes");
-Tags = new Meteor.Collection("tags");
-
-
-Meteor.startup(function () {
-  Session.set("selectedTag", null);
-
-}); // end of startup
-
-
-
-// Handlebars if eq helper definition
-Handlebars.registerHelper('if_eq', function (ownerId, opts) {
-  if (ownerId === Meteor.userId()) {
-    return opts.fn(this);
-  } else {
-    return opts.inverse(this);
-  }
-});
-
-Meteor.autorun(function () {
-
-  if (Meteor.userId()) {
-    Session.set('userId', Meteor.userId());
-    setTimeout(function () {
-      $('#noteTag').tagsinput('refresh');
-    }, 200)
-  } else {
-    Session.set('userId', '');
-  }
-});
-
-Template.viewNotes.notes = function () {
-  var selectedTag = Session.get("selectedTag");
-  return  selectedTag ? Notes.find({tags: selectedTag}, {sort: {time: -1}}) : Notes.find({}, {sort: {time: -1}});
+// rendered goes here
+Template.inputForm.rendered = function(){
+  $('#noteTag').tagsinput({
+    maxTags: 3,
+  });
 }
 
-Template.viewNotes.localTime = function () {
-  var utcTime = new Date(this.time);
-  var formattedStr = formatAMPM(utcTime)
-  return formattedStr;
-};
+// helpers goes here
+Template.input_note.helpers({
+  templateName: function(){
+    return Session.get('current_template')
+  },
 
-var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
-
-var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
-
-function formatAMPM(dateObj) {
-  var day = dayNames[dateObj.getDay()];
-  var date = dateObj.getDate();
-  var month = monthNames[dateObj.getMonth()];
-  var year = dateObj.getFullYear();
-  var hours = dateObj.getHours();
-  var minutes = dateObj.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  var strTime = day + ", " + month + " " + date + " " + year + ", " + hours + ':' + minutes + ' ' + ampm;
-  return strTime;
-}
-
-Template.tagCloud.availableTags = function () {
-  return Tags.find({}, {sort: {count: -1}});
-};
-
-Template.tagAlert.selectedTag = function () {
-  return Session.get('selectedTag');
-};
-
-Template.tagAlert.events = {
-  'click .all': function (event) {
-    Session.set('selectedTag', '');
+  isLoggedIn: function () {
+    return Meteor.userId();
   }
-}
+})
 
-Template.tagCloud.events = {
+Template.viewNotes.helpers({
+  notes: function() {
+    var selectedTag = Session.get("selectedTag");
+    return  selectedTag ? Notes.find({tags: selectedTag}, {sort: {time: -1}}) : Notes.find({}, {sort: {time: -1}});
+  },
+
+  localTime: function() {
+    var utcTime = new Date(this.time);
+    var formattedStr = formatAMPM(utcTime)
+    return formattedStr;
+  },
+
+  selectedTag: function () {
+    return Session.get('selectedTag');
+  },
+
+})
+
+Template.tagCloud.helpers({
+  availableTags: function () {
+    return Tags.find({}, {sort: {count: -1}});
+  }
+})
+
+Template.tagCloud.events ({
   'click .tgname': function (event) {
     var anch = $(event.currentTarget);
     var asd = anch.find('.tagName');
     Session.set("selectedTag", asd.text().trim());
   }
-}
-Template.viewNotes.events = {
+})
+
+Template.viewNotes.events ({
+  'click .all': function (event) {
+    Session.set('selectedTag', '');
+  },
+
   'click .del': function (event) {
     var id = this._id;
     if (id) {
@@ -110,52 +80,60 @@ Template.viewNotes.events = {
 //        t.addClass('hide');
   }
 
-}
-Template.input_note.isLoggedIn = function () {
-  return Meteor.userId();
-};
+})
 
-Template.inputForm.rendered = function(){
-  $('#noteTag').tagsinput({
-    maxTags: 3,
-
-  });
-}
-
-Template.inputForm.events = {
-  'click #btn': function (event) {
-    saveNotes(event)
-  },
+Template.inputForm.events({
   'keydown input#noteText': function (event) {
     if (event.which == 13) { // 13 is the enter key event
-      saveNotes(event)
+      $("#btn").trigger('click')
     }
-  }
-};
+  },
 
-function saveNotes(event) {
+  'click #btn': function (event) {
+    var noteText = $('#noteText');
+    var $noteTag = $('#noteTag');
 
-  var noteText = $('#noteText');
-  var $noteTag = $('#noteTag');
-
-  if (noteText.val() != '') {
-    var nodeTag = $noteTag.val();
-    tagsArr = nodeTag.split(',');
-    if (tagsArr.length <= 3) {
-      var ownerId = Session.get('userId');
-      nodeTag = nodeTag === '' ? null : nodeTag;
-      Meteor.call('saveNote', noteText.val(), nodeTag, ownerId);
-      noteText.val('');
-      $noteTag.val('');
-      $noteTag.tagsinput('removeAll');
+    if (noteText.val() != '') {
+      var nodeTag = $noteTag.val();
+      tagsArr = nodeTag.split(',');
+      if (tagsArr.length <= 3) {
+        var ownerId = Session.get('userId');
+        nodeTag = nodeTag === '' ? 'untagged' : nodeTag;
+        Meteor.call('saveNote', noteText.val(), nodeTag, ownerId);
+        noteText.val('');
+        $noteTag.val('');
+        $noteTag.tagsinput('removeAll');
+      } else {
+        $noteTag.val('');
+        $noteTag.tagsinput('removeAll');
+        $noteTag.attr("placeholder", "Only upto 3 tags are allowed....!");
+      }
+      noteText.attr("placeholder", "Enter the note details");
     } else {
-      $noteTag.val('');
-      $noteTag.tagsinput('removeAll');
-      $noteTag.attr("placeholder", "Only upto 3 tags are allowed....!");
+      noteText.attr("placeholder", "Notes with empty text not allowed...!");
     }
-    noteText.attr("placeholder", "Enter the note details");
-  } else {
-    noteText.attr("placeholder", "Notes with empty text not allowed...!");
   }
-}
+});
 
+
+
+
+// function required are put here
+var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+
+function formatAMPM(dateObj) {
+  var day = dayNames[dateObj.getDay()];
+  var date = dateObj.getDate();
+  var month = monthNames[dateObj.getMonth()];
+  var year = dateObj.getFullYear();
+  var hours = dateObj.getHours();
+  var minutes = dateObj.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = day + ", " + month + " " + date + " " + year + ", " + hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
